fe290105d8735b1825c6743215ed3a995ef05575
Thorben <t0astbr0d@gmx.de>
2018-12-02 15:28:32 +0100

    new line

--- src/de_DE/Start.java
+++ src/de_DE/Start.java
@@ -5,6 +5,7 @@ public class Start {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Neue Zeile");
+		System.out.println("Jetzt Develop");
 	}
 
 }
